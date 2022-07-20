d9420403e6f7762ff806a9d4edc60bb7ed02ec57
tjcanger <2052717@tongji.edu.cn>
2022-07-20 16:35:23 +0800

    修改点菜主界面

--- you-are-what-you-eat-admin/pages/orderdishes/index.tsx
+++ you-are-what-you-eat-admin/pages/orderdishes/index.tsx
@@ -1,53 +1,90 @@
-import Head from 'next/head';
-
 import SidebarLayout from '@/layouts/SidebarLayout';
 
-import PageHeader from '@/content/Dashboards/Crypto/PageHeader';
-import PageTitleWrapper from '@/components/PageTitleWrapper';
-import { Container, Grid } from '@mui/material';
-import Footer from '@/components/Footer';
+import { Grid,Box,Stack } from '@mui/material';
 
-import AccountBalance from '@/content/Dashboards/Crypto/AccountBalance';
-import Wallets from '@/content/Dashboards/Crypto/Wallets';
-import AccountSecurity from '@/content/Dashboards/Crypto/AccountSecurity';
-import WatchList from '@/content/Dashboards/Crypto/WatchList';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+interface DishProps{
+    dishname:string;
+    description:string;
+}
 
-function DashboardCrypto() {
+const InitialDish=():Array<DishProps>=>{
+return [
+    {
+        dishname:"清炒土豆丝",
+        description:"简单的做法，极致的美味"
+    },
+    {
+        dishname:"番茄炒蛋",
+        description:"有点甜"
+    },
+    {
+        dishname:"炒洋葱",
+        description:"那是相当难吃"
+    },
+    {
+        dishname:"歪比巴卜",
+        description:"僵尸吃掉了你的脑子！"
+    },
+    {
+        dishname:"歪比巴卜",
+        description:"僵尸吃掉了你的脑子！"
+    },
+    {
+        dishname:"歪比巴卜",
+        description:"僵尸吃掉了你的脑子！"
+    }
+]
+}
+
+function Dishpanel(dishes:Array<DishProps>){
+
+dishes=InitialDish();
+
   return (
-    <>
-      <Head>
-        <title>Crypto Dashboard</title>
-      </Head>
-      <PageTitleWrapper>
-        <PageHeader />
-      </PageTitleWrapper>
-      <Container maxWidth="lg">
-        <Grid
-          container
-          direction="row"
-          justifyContent="center"
-          alignItems="stretch"
-          spacing={4}
-        >
-          <Grid item xs={12}>
-            <AccountBalance />
-          </Grid>
-          <Grid item lg={8} xs={12}>
-            <Wallets />
-          </Grid>
-          <Grid item lg={4} xs={12}>
-            <AccountSecurity />
-          </Grid>
-          <Grid item xs={12}>
-            <WatchList />
-          </Grid>
-        </Grid>
-      </Container>
-      <Footer />
-    </>
+<Box sx={{minWidth:100}}>
+ <Grid container spacing={1}> 
+    {
+        dishes.map((dish:DishProps)=>{
+        return (
+      <Grid item xs={4} >
+        <Card sx={{ minWidth:320 }} >
+      <CardMedia
+        component="img"
+        height="140" 
+        image="public\static\images\dishimg\potato.png"
+        alt="potatochips"
+      />
+      <CardContent>
+        
+        <Typography gutterBottom variant="h5" component="div">
+          {dish.dishname}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {dish.description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">点餐</Button>
+        <Button size="small">查看详细评价</Button>
+      </CardActions>
+     </Card> </Grid>
+     );
+  
+     })
+    }
+    </Grid> 
+    </Box>
   );
 }
 
-DashboardCrypto.getLayout = (page) => <SidebarLayout>{page}</SidebarLayout>;
 
-export default DashboardCrypto;
+ Dishpanel.getLayout = (page) => <SidebarLayout>{page}</SidebarLayout>;
+
+export default Dishpanel;
